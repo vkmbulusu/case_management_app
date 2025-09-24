f42683d02bedbb3c919f1498a4789976dbbc2695
Krishna Mohan Bulusu <vkmbulusu@outlook.com>
2025-09-24 11:36:52 +0530

    Create db.py

---
+++ db.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+# SQLite database
+DATABASE_URL = "sqlite:///cases.db"
+engine = create_engine(DATABASE_URL, echo=False)
+
+# Session factory
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+def create_db():
+    from models import Base
+    Base.metadata.create_all(bind=engine)
